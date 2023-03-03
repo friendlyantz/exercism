a23a0db1307071490ef08c27af3f9551d7200761
Anton Panteleev <friendlyantz@pm.me>
2023-03-04 09:47:32 +1100

    Refactor and simplify conditioknals

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -2,15 +2,17 @@ class PhoneNumber
   def self.clean(number)
     n = number.gsub(/[^0-9]/, '')
 
-    return if n.size <= 9 || n.size > 11
-    return if (%w[0 1] & [n[-10], n[-7]]).any?
+    return unless (10..11).cover? n.size
+
+    return if %w[0 1].include? n[-10]
+    return if %w[0 1].include? n[-7]
 
     if n.size == 11
       return if n[-10] == '0'
 
       return unless n[0] == '1'
 
-      n = n[1..-1]
+      n = n[1..]
     end
     n
   end
