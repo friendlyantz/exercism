835994603d97b1b4366d954ce46d615d6f51fdb2
Anton Panteleev <friendlyantz@pm.me>
2023-03-04 00:44:53 +1100

    Refac microwave

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -9,11 +9,10 @@ class Microwave
     if time.to_s.size <= 2
       min, sec = time.divmod(60)
     else
-      sec = time.to_s[-2..-1].to_i
-      min = time.to_s[0...-2].to_i
+      min, sec = time.to_s[0...-2].to_i, time.to_s[-2..-1].to_i
       if sec >= 60
-        sec = sec % 60
         min += 1
+        sec = sec % 60
       end
     end
     "#{format_time(min)}:#{format_time(sec)}"
