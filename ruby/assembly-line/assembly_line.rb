9a53bb9c84440a24f0bbfb7defd5fed625d0d54b
Anton Panteleev <friendlyantz@pm.me>
2023-03-24 22:30:01 +1100

    Solved with 'floor' rounding

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,13 +1,27 @@
 class AssemblyLine
+  HOURLY_OUTPUT = 221
+
+  MODULATOR = {
+    1 => 1,
+    2 => 1,
+    3 => 1,
+    4 => 1,
+    5 => 0.9,
+    6 => 0.9,
+    7 => 0.9,
+    8 => 0.9,
+    9 => 0.8,
+    10 => 0.77
+  }
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    HOURLY_OUTPUT * @speed * MODULATOR[@speed]
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60.0).floor
   end
 end
