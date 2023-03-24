18d7a6400b7224284d2f1eba94fc055af29eaa2d
Anton Panteleev <friendlyantz@pm.me>
2023-03-24 17:53:41 +1100

    Solved, but only due to community solutions inspiration

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,46 +1,29 @@
+class RobotSerial
+  def initialize
+    @serial = 'AA000'
+  end
+
+  def generate_name
+    name = @serial.dup
+    @serial.next!
+    name
+  end
+end
+
 class Robot
   attr_reader :name
 
-  USED_NAMES = []
+  @@serializer = RobotSerial.new
 
   def initialize
-   @name = generate_name(USED_NAMES.last)
+    reset
   end
 
-  def self.forget; end
+  def self.forget
+    @@serializer = RobotSerial.new
+  end
 
   def reset
-    @name = generate_name(USED_NAMES.last)
-  end
-
-  private
-
-  def generate_name(starting_string)
-    # Separate the letters and digits
-    # binding.pry
-    starting_string = starting_string || 'AA000'
-    letters = starting_string[0..1]
-    digits = starting_string[2..-1].to_i
-
-    # Increment the digits
-    digits += 1
-
-    # Check if we need to increment the letters
-    if digits == 1000
-      digits = 0
-      letters = letters.next.next
-    end
-
-    # Combine the letters and digits
-    
-    name =  "#{letters}#{format('%03d', digits)}"
-    
-    # binding.pry
-    
-    while USED_NAMES.include?(name)
-      name = generate_name(name)
-    end
-    USED_NAMES << name
-    name
+    @name = @@serializer.generate_name
   end
 end
