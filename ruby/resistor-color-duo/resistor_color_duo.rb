a57ee5995d860fed8bff615d66edcd1005d54a3d
Anton Panteleev <friendlyantz@pm.me>
2023-03-26 10:08:27 +1100

    Solved

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class ResistorColorDuo
+  COLOR_MAP = {
+    Black: 0,
+    Brown: 1,
+    Red: 2,
+    Orange: 3,
+    Yellow: 4,
+    Green: 5,
+    Blue: 6,
+    Violet: 7,
+    Grey: 8,
+    White: 9
+  }
+              .transform_keys { |k| k.to_s.downcase }
+              .transform_values { |v| v.to_s.downcase }
+              .freeze
+
+  def self.value(input)
+    input
+      .first(2)
+      .map { |color| COLOR_MAP[color] }
+      .join
+      .to_i
+  end
+end
