1939d6fc0a5f3512f5a4e9c3b060384a21e35791
Anton Panteleev <friendlyantz@pm.me>
2023-04-03 21:15:06 +1000

    Refactored using file read

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,20 +1,32 @@
-class ResistorColorDuo
-  COLOR_MAP = {
-    Black: 0,
-    Brown: 1,
-    Red: 2,
-    Orange: 3,
-    Yellow: 4,
-    Green: 5,
-    Blue: 6,
-    Violet: 7,
-    Grey: 8,
-    White: 9
-  }
-              .map { |k,v| [k.to_s.downcase, v.to_s.downcase]}.to_h
-              .freeze
+# RAW_INPUT = File.read(File.join(__dir__, 'input'))
 
+# File.open('./preped_input.rb', 'w') do |file|
+#   file.write(
+#     'BAND_INDEX = ' + RAW_INPUT
+#         .split("\n")
+#         .map { |line| line.split(': ') }.to_h
+#         .transform_keys { |k| k.to_s.downcase }
+#         .inspect
+#   )
+# end
+
+# require './preped_input'
+BAND_INDEX = {
+  'black' => '0',
+  'brown' => '1',
+  'red' => '2',
+  'orange' => '3',
+  'yellow' => '4',
+  'green' => '5',
+  'blue' => '6',
+  'violet' => '7',
+  'grey' => '8',
+  'white' => '9'
+}
+
+class ResistorColorDuo
   def self.value(colour_pair)
-    (COLOR_MAP[colour_pair[0]] + COLOR_MAP[colour_pair[1]]).to_i
+    first_colour, second_colour = colour_pair.first(2)
+    (BAND_INDEX[first_colour] + BAND_INDEX[second_colour]).to_i
   end
 end
