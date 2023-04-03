5972ac9075307ec0088ce6a689a398e6b24bf0e8
Anton Panteleev <friendlyantz@pm.me>
2023-04-03 14:43:06 +1000

    1st case solved(teenth monday)
    Co-authored-by: Kieran O'Grady <kieranjogrady@users.noreply.github.com>

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -7,17 +7,32 @@ class Meetup
     @year = year
   end
 
-  POSITIONS = [
-   "first", "second", "third", "fourth", "last", "teenth" 
-  ]
-  def day(weekday, weekday_pos)
-    case weekday_pos
-    when :teenth
-      binding.pry
-    end
-    day = 1
-    Date.parse("#{day}-#{@month}-#{@year}")
-    # Date.parse('formatted string DD-MM-YYYY')
-  end
+  WEEKDAY_NUMS = {
+    sunday: 0,
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6
+  }
 
+  POSITIONS = {
+    first: 0,
+    second: (7 - 1),
+    third: (14 - 1),
+    fourth: (21 - 1),
+    last: -> { 'ladida' },
+    teenth: (12 - 1)
+  }
+
+  def day(weekday, weekday_pos)
+    date = Date.new(@year, @month)
+    day = WEEKDAY_NUMS[weekday]
+    pos = POSITIONS[weekday_pos]
+
+    date += pos
+    date = date.next_day until date.wday == day
+    date
+  end
 end
