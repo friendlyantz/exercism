0724f4f9dd2232d5cc22cd4de6d3df22751b06e4
Anton Panteleev <friendlyantz@pm.me>
2023-04-05 13:41:39 +1000

    solved all

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,7 +1,14 @@
-=begin
-Write your code for the 'Word Count' exercise in this file. Make the tests in
-`word_count_test.rb` pass.
+class Phrase
+  def initialize(phrase)
+    @phrase = phrase
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/word-count` directory.
-=end
+  def word_count
+    @phrase
+      .downcase
+      .scan(/\w+'\w+|\w+/)
+      .each_with_object(Hash.new(0)) do |word, hash|
+        hash[word] += 1
+      end
+  end
+end
