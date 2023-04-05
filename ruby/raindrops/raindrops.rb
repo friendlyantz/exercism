ee23c29ec152985eec25532b9e9ad437d6008588
Anton Panteleev <friendlyantz@pm.me>
2023-04-05 19:56:11 +1000

    Use class instead of module

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,18 +1,18 @@
-module Raindrops
-  module_function
-
+class Raindrops
   WORD_INDEX = {
     3 => 'Pling',
     5 => 'Plang',
     7 => 'Plong'
   }.freeze
 
-  def convert(number)
-    result = WORD_INDEX
-             .filter { |factor, _sound| (number % factor).zero? }
-             .values
-             .join
+  class << self
+    def convert(number)
+      result = WORD_INDEX
+               .filter { |factor, _sound| (number % factor).zero? }
+               .values
+               .join
 
-    result.empty? ? number.to_s : result
+      result.empty? ? number.to_s : result
+    end
   end
 end
