d3548b96e41ba849dfffc4a42a0ff6adc3899d2d
Anton Panteleev <friendlyantz@pm.me>
2023-04-05 12:25:07 +1000

    Solved using integers as values and instance

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -2,19 +2,28 @@ RAW_INPUT = File.read(File.join(__dir__, 'input'))
 
 File.open('./preped_input.rb', 'w') do |file|
   file.write(
-    'BAND_INDEX = ' + RAW_INPUT
-        .split("\n")
-        .map { |line| line.split(': ') }.to_h
-        .transform_keys { |k| k.to_s.downcase }
-        .inspect
+    'BAND_INDEX = '.concat(
+      RAW_INPUT
+      .lines
+      .map { |line| line.split(': ') }.to_h
+      .map { |k, v| [k.downcase, v.to_i] }.to_h
+      .inspect
+    )
   )
 end
 
 require './preped_input'
 
 class ResistorColorDuo
-  def self.value(colour_pair)
-    first_colour, second_colour = colour_pair.first(2)
-    (BAND_INDEX[first_colour] + BAND_INDEX[second_colour]).to_i
+  def self.value(colors)
+    new(colors).to_i
+  end
+
+  def initialize(colour_pair)
+    @first_colour, @second_colour = colour_pair.first(2)
+  end
+
+  def to_i
+    "#{BAND_INDEX[@first_colour]}#{BAND_INDEX[@second_colour]}".to_i
   end
 end
