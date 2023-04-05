ade11af58a6b984b0575097f246cd23c808aa88d
Anton Panteleev <friendlyantz@pm.me>
2023-04-05 12:04:29 +1000

    prepped input formatted

--- ruby/resistor-color-duo/preped_input.rb
+++ ruby/resistor-color-duo/preped_input.rb
@@ -1,1 +1,12 @@
-COLOR_MAP = {"black"=>"0", "brown"=>"1", "red"=>"2", "orange"=>"3", "yellow"=>"4", "green"=>"5", "blue"=>"6", "violet"=>"7", "grey"=>"8", "white"=>"9"}
+BAND_INDEX = {
+  'black' => 0,
+  'brown' => 1,
+  'red' => 2,
+  'orange' => 3,
+  'yellow' => 4,
+  'green' => 5,
+  'blue' => 6,
+  'violet' => 7,
+  'grey' => 8,
+  'white' => 9
+}
