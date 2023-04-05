7fd52d5ba53d381ba36b4a7cee8bd38dd7fcc2db
Anton Panteleev <friendlyantz@pm.me>
2023-04-05 19:47:14 +1000

    refactor

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,11 +1,17 @@
 module Raindrops
   module_function
 
+  WORD_INDEX = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }
+
   def convert(number)
     result = ''
-    result << 'Pling' if number % 3 == 0
-    result << 'Plang' if number % 5 == 0
-    result << 'Plong' if number % 7 == 0
+    WORD_INDEX.each do |factor, sound|
+      result << sound if (number % factor).zero?
+    end
     result.empty? ? number.to_s : result
   end
 end
