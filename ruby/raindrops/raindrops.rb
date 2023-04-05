f0b1646858c3d50913ed82c798d38d300444e60c
Anton Panteleev <friendlyantz@pm.me>
2023-04-05 19:54:04 +1000

    Refactor with filtering hash

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -5,13 +5,14 @@ module Raindrops
     3 => 'Pling',
     5 => 'Plang',
     7 => 'Plong'
-  }
+  }.freeze
 
   def convert(number)
-    result = ''
-    WORD_INDEX.each do |factor, sound|
-      result << sound if (number % factor).zero?
-    end
+    result = WORD_INDEX
+             .filter { |factor, _sound| (number % factor).zero? }
+             .values
+             .join
+
     result.empty? ? number.to_s : result
   end
 end
