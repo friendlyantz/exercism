4e39a195c3eaa9a38317228c27cffcd10710f66c
Anton Panteleev <friendlyantz@pm.me>
2023-04-05 13:56:33 +1000

    Refactor

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,11 +1,11 @@
-require 'pry'
-
 module Hamming
   module_function
 
   def compute(strand1, strand2)
     raise ArgumentError, 'Strands must be the same length' if strand1.length != strand2.length
 
-    strand1.chars.zip(strand2.chars).count { |a, b| a != b }
+    strand1.chars
+           .zip(strand2.chars)
+           .count { |a, b| a != b }
   end
 end
