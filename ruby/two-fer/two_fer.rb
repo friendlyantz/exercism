7053a93339480d86e84448fe65c14d59847de240
Anton Panteleev <friendlyantz@pm.me>
2023-04-07 08:55:33 +1000

    overriding `self` method

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -2,16 +2,15 @@ class TwoFer
   SENTENCE = 'One for %(name)s, one for me.'.freeze
 
   def self.two_fer(name = 'you')
-    self.new(name).to_s
+    new(name).to_s
   end
 
-  attr_reader :name
-
-  def initialize(name)
+  def self.new(name)
     @name = name
+    self
   end
 
-  def to_s
-    "One for %<name>s, one for me." % {name: name}
+  def self.to_s
+    format('One for %<name>s, one for me.', name: @name)
   end
 end
