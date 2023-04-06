1b712f528570fe6400159ee43198a5ad2b5ec7c5
Anton Panteleev <friendlyantz@pm.me>
2023-04-07 08:48:33 +1000

    Implement class instance method

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,7 +1,17 @@
-module TwoFer
-  extend self
+class TwoFer
+  SENTENCE = 'One for %(name)s, one for me.'.freeze
 
-  def two_fer(name = 'you')
-    'One for %s, one for me.' % name
+  def self.two_fer(name = 'you')
+    self.new(name).to_s
+  end
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "One for %<name>s, one for me." % {name: name}
   end
 end
