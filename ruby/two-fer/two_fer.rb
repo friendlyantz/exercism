2b0e7d225e1a8254e5b63f912593f659490cf6e9
Anton Panteleev <friendlyantz@pm.me>
2023-04-07 07:45:24 +1000

    replace module_function. String format changed
    module_function is not ideal. The methods in a module may be instance
    methods or module methods. Instance methods appear as methods in a class
    when the module is included, module methods do not

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,7 +1,7 @@
 module TwoFer
-  module_function
+  extend self
 
-  def two_fer(name = "you")
-    "One for #{name}, one for me."
+  def two_fer(name = 'you')
+    'One for %s, one for me.' % name
   end
 end
