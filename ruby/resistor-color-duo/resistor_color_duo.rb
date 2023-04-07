0b44433296f3f704e136d7060b532c5eb945be4b
Anton Panteleev <friendlyantz@pm.me>
2023-04-07 19:33:27 +1000

    refactor to use only 1 instance variable

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,7 @@
 require_relative './band'
 
 class ResistorColorDuo
-  attr_reader :first_value, :second_value
+  attr_reader :value
 
   def self.value(colors)
     new(colors).to_i
@@ -10,12 +10,11 @@ class ResistorColorDuo
   def initialize(colour_pair)
     colour_pair
       .then do |first_colour, second_colour|
-        @first_value = BAND[first_colour].to_s
-        @second_value = BAND[second_colour].to_s
+        @value = BAND[first_colour].to_s + BAND[second_colour].to_s
       end
   end
 
   def to_i
-    (first_value + second_value).to_i
+    value.to_i
   end
 end
