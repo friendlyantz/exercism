ae180ec252ebd175e889b012d8d84804bb4d437b
Anton Panteleev <friendlyantz@pm.me>
2023-04-09 09:56:40 +1000

    Reformat band to align

--- ruby/resistor-color-duo/band.rb
+++ ruby/resistor-color-duo/band.rb
@@ -1,12 +1,12 @@
 BAND = {
-  'black' => 0,
-  'brown' => 1,
-  'red' => 2,
-  'orange' => 3,
-  'yellow' => 4,
-  'green' => 5,
-  'blue' => 6,
-  'violet' => 7,
-  'grey' => 8,
-  'white' => 9
+  'black'   => 0,
+  'brown'   => 1,
+  'red'     => 2,
+  'orange'  => 3,
+  'yellow'  => 4,
+  'green'   => 5,
+  'blue'    => 6,
+  'violet'  => 7,
+  'grey'    => 8,
+  'white'   => 9
 }
