c9b45b75fc6aed44bdc6afe193e2701d3948f791
Anton Panteleev <friendlyantz@pm.me>
2023-04-08 19:47:48 +1000

    Refac using 'initialize' instead of 'new'. fix typo
    typo with constant string '<name>'
    
    Also, note that there is no default in the instance level call, which is very nice. If a name is required for the "power user" then that is great.
    
    The class level method is there for convenience only, and it should stay inflexible, as it is there for convenience not power. We will see this decision in use in our "expansion", below.

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,16 +1,15 @@
 class TwoFer
-  SENTENCE = 'One for %(name)s, one for me.'.freeze
+  SENTENCE = 'One for %<name>s, one for me.'.freeze
 
   def self.two_fer(name = 'you')
     new(name).to_s
   end
 
-  def self.new(name)
+  def initialize(name)
     @name = name
-    self
   end
 
-  def self.to_s
-    format('One for %<name>s, one for me.', name: @name)
+  def to_s
+    SENTENCE % { name: @name }
   end
 end
