de40318953d6bd3f78ab18cdd8e7147177554944
Anton Panteleev <friendlyantz@pm.me>
2023-04-09 11:42:17 +1000

    Privatise att_readers for 'to_s'

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -7,22 +7,21 @@ class ResistorColorDuo
 
   private
 
+  attr_reader :first_color, :second_color
   attr_reader :value
 
   DISPLAY = '%<tens>s%<ones>s'
 
   def initialize(color_pair)
     @first_color, @second_color = color_pair
-    @value = self.to_s
+    @value = to_s
   end
-
-  public
-
-  attr_reader :first_color, :second_color
 
   def to_s
     DISPLAY % { tens: BAND[first_color], ones: BAND[second_color] }
   end
+
+  public
 
   def to_i
     value.to_i
